c474b71e7d58e6857e8f7095e9cc366cf8551b39
Abolfazl <abtarahomii@gmail.com>
2024-11-17 19:01:05 +0330

    create and add Header Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='bg-[#5964E0] h-32 flex justify-between items-center'>
+        <div><span className='text-white text-[30px] font-bold m-10 '>devjobs</span></div>
+        <div>
+             <p>light and dark mode</p>
+        </div>
+
+    </div>
+  )
+}
+
+export default Header
