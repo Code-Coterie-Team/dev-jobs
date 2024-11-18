1c9a9e29b3fe9b88e6ec2fe7214eb30f379efa8d
Abolfazl <abtarahomii@gmail.com>
2024-11-18 13:21:36 +0330

    add header svg

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,15 @@
 import React from "react";
-// import svgimage from 'src\assets\images\bg-pattern-header.svg'
 
 const Header = () => {
   return (
-    
-    
-      <div className=" relative bg-[url('src\assets\images\bg-pattern-header.svg')]">
-         
-      
-      <div>
+    <div className="h-32 flex justify-between items-center rounded-bl-[90px] bg-slate-900 relative">
+      <img className="w-full h-full border" src="src\assets\images\bg-pattern-header.svg"></img>
+      <div className="flex justify-between  items-center absolute ">
         <span className="text-white text-[30px] font-bold m-10 ">devjobs</span>
-      </div>
-      <div>
         <p className="font-bold m-10 ">light and dark mode</p>
       </div>
-      </div>
+    </div>
   );
 };
 
 export default Header;
-
-
-{/* <div
-className="h-32 flex justify-between items-center rounded-bl-[90px]"
-style={{ backgroundImage: `url(${svgimage})`}}
-> */}
