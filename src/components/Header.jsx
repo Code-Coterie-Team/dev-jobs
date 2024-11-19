f967db675f9cdb82dcbb0d1db6b8ef895dabbd36
Abolfazl <abtarahomii@gmail.com>
2024-11-19 13:05:48 +0330

    add UseState

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import ToggleMode from "../../Hooks/Toggleapp";
 
 const Header = () => {
+  const[colorTheme,setTheme] = ToggleMode();
+  const [darkSide,setDarkSide] = useState(colorTheme === 'light' ? true : false);
+  
+  
   return (
     <div className="h-32 flex justify-between items-center  relative">
       <img className="w-full h-full border rounded-bl-full" src="src\assets\images\bg-pattern-header.svg"></img>
