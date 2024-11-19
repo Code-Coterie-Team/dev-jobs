266eee549fb40fe7c27d58e817f5d54de31d8253
Abolfazl <abtarahomii@gmail.com>
2024-11-19 10:13:16 +0330

    check header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="h-32 flex justify-between items-center rounded-bl-[90px] bg-slate-900 relative">
-      <img className="w-full h-full border" src="src\assets\images\bg-pattern-header.svg"></img>
+    <div className="h-32 flex justify-between items-center  relative">
+      <img className="w-full h-full border rounded-bl-full" src="src\assets\images\bg-pattern-header.svg"></img>
       <div className="flex justify-between  items-center absolute ">
         <span className=" text-[30px] font-bold m-10 ">devjobs</span>
         <p className="font-bold m-10 ">light and dark mode</p>
