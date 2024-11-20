4279972aa1e28e7aae62dfe6841fff8a4a1f4eed
Abolfazl <abtarahomii@gmail.com>
2024-11-20 11:33:41 +0330

    solve & Fix bug in Paternsvg

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,17 @@
-import React, { useState } from "react";
-
-
+import React from "react";
+import mySvg from '../assets/images/sun.svg'; // مسیر صحیح
+import patternSvg from '../assets/images/bg-pattern-header.svg'; // بارگیری تصویر به روش import
 
 const ThemeToggleButton = () => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-
-  const toggleTheme = () => {
-      setIsDarkMode(prev => !prev);
-  };
-
-
-
   return (
-    <div className="h-32 flex justify-between items-center  relative">
-      <img className="w-full h-full border rounded-bl-full" src="src\assets\images\bg-pattern-header.svg"></img>
-      <div className="flex justify-between  items-center absolute ">
-        <span className=" text-[30px] font-bold m-10 ">devjobs</span>
-           
+    <div className="h-32 flex justify-between items-center relative">
+      <img className="w-full h-full border rounded-bl-full" src={patternSvg} alt="Background Pattern" />
+      <div className="absolute left-0 right-0 flex justify-between items-center px-10"> {/* اضافه کردن padding به صورت کناری */}
+        <span className="text-[30px] font-bold">devjobs</span>
+        <img src={mySvg} alt="Sun" className="h-8 w-8" /> {/* می‌توانید اندازه تصویر را با کلاس‌های Tailwind تنظیم کنید */}
       </div>
     </div>
   );
-
-
-};
+}
 
 export default ThemeToggleButton;
