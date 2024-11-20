bdf85c8787d1ef01f24fe717c8d8519bea9e3cec
Abolfazl <abtarahomii@gmail.com>
2024-11-20 12:04:43 +0330

    set flex elements

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import mySvg from '../assets/images/sun.svg'; // مسیر صحیح
 import patternSvg from '../assets/images/bg-pattern-header.svg'; // بارگیری تصویر به روش import
 import halfmoon from '../assets/images/moon.svg'; // اصلاح نام فایل و پسوند
-
+import chang from '../assets/images/menu.svg'; // اصلاح نام فایل و پسوند
 
 const ThemeToggleButton = () => {
   return (
@@ -12,6 +12,7 @@ const ThemeToggleButton = () => {
         <span className="text-[30px] font-bold">devjobs</span>
         <div className="flex">
         <img src={mySvg} alt="Sun" className="h-5 w-5 filter brightness-0 invert" /> {/* تغییر رنگ به سفید */}
+        <img src={chang} alt="Sun" className="h-5 w-5 filter brightness-0 invert" /> 
         <img src={halfmoon} alt="Moon" className="h-5 w-5 filter brightness-0 invert" /> {/* اضافه کردن کلاس اندازه به تصویر ماه */}
         </div>
       </div>
