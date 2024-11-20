64018644b5466906f121b81e08f28484e742183f
Abolfazl <abtarahomii@gmail.com>
2024-11-20 11:46:47 +0330

    add white Color to sun svg0

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,9 +6,9 @@ const ThemeToggleButton = () => {
   return (
     <div className="h-32 flex justify-between items-center relative">
       <img className="w-full h-full border rounded-bl-full" src={patternSvg} alt="Background Pattern" />
-      <div className="absolute left-0 right-0 flex justify-between items-center px-10"> {/* اضافه کردن padding به صورت کناری */}
+      <div className="absolute left-0 right-0 flex justify-between items-center px-10">
         <span className="text-[30px] font-bold">devjobs</span>
-        <img src={mySvg} alt="Sun" className="h-8 w-8" /> {/* می‌توانید اندازه تصویر را با کلاس‌های Tailwind تنظیم کنید */}
+        <img src={mySvg} alt="Sun" className="h-8 w-8 filter brightness-0 invert" /> {/* تغییر رنگ به سفید */}
       </div>
     </div>
   );
