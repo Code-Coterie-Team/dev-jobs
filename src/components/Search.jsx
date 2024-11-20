667a5ff6f46eb1d6c722d1d0411fee5b0fa0601f
Abolfazl <abtarahomii@gmail.com>
2024-11-20 19:20:02 +0330

    add btn component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -51,7 +51,7 @@ const Search = () => {
           </div>
 
           <button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
-            Button
+            Serach
           </button>
         </div>
       </div>
