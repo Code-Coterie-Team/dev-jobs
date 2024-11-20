074eb5cf7907fd02546020c66343fc3343954aa7
Abolfazl <abtarahomii@gmail.com>
2024-11-20 13:10:27 +0330

    add location svg

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,25 +1,27 @@
 import React from "react";
 import search from "../assets/images/searchN.svg"; // اصلاح نام فایل و پسوند
-
+import location from "../assets/images/location.svg";
 const Search = () => {
   return (
     <div className="flex justify-center -translate-y-1/2 flex-row">
       <div className="bg-white text-red-400 flex justify-center items-center gap-24 flex-row w-[80%] mx-[10%] h-14 rounded-md border-none">
         <div className="flex justify-between items-center basis-1/2 m-2 p-2 border-r-2 border-[#E2E6EA]">
-          <img 
-            src={search} 
-            alt="Menu" 
-            className="h-8 w-9 filter hue-rotate-190" /> {/* تغییر رنگ به آبی */}
-          
-          <input 
-            type="text" 
+          <img
+            src={search}
+            alt="Menu"
+            className="h-8 w-9 filter hue-rotate-190"
+          />{" "}
+          {/* تغییر رنگ به آبی */}
+          <input
+            type="text"
             placeholder="Filter by title, companies,..."
-            className="w-full h-[100%] rounded-md m-5 border-none outline-none" 
+            className="w-full h-[100%] rounded-md m-5 border-none outline-none"
           />
         </div>
 
+        <img src={location} alt="location" className="h-8 w-9" />
         <select className="basis-1/4 border-r-2 border-[#E2E6EA]">
-          <option>Other option</option>
+          <option>Filter By Location</option>
           <option>Some option</option>
         </select>
 
